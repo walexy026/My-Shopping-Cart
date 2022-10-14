c93ecaa891b15ac3ed1eecefa30ac6a011e7a14e
olawale <jao.olaniyi@gmail.com>
2022-10-14 16:58:18 +0100

    display the product sizing

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -13,9 +13,9 @@ const ProductCard = (props) => {
     <Card style={{ width: '18rem', height:'auto'}}
     className={`text-center p-0 overflow-hidden shadow mx-auto mb-4`}
     >
-        <div style={{background:'white', height:'15rem', overflow:'hidden', display:'flex', justifyContent:'center', alignItems:'center'}}>
-            <div>
-      <Card.Img variant="top" src={image} />
+        <div style={{background:'white', height:'15rem', overflow:'hidden', display:'flex', justifyContent:'center', alignItems:'center', marginBottom:'inherit'}}>
+            <div style={{width:'9rem'}}>
+      <Card.Img variant="top" src={image} className='img-fluid'/>
             </div>
         </div>
       <Card.Body>
