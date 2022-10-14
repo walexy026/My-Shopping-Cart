d7f08241db863ebfebb7ffad68fceba23b5896c5
olawale <jao.olaniyi@gmail.com>
2022-10-14 19:43:15 +0100

    the addition and subtraction of cart added

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -20,7 +20,41 @@ const Cart = () => {
         {isEmpty? 'Your Cart is Empty': 'The Cart'}
         </h1>
         <Row className="justify-content-center">
-
+        <Table responsive="sm" className="mb-5"  striped bordered hover variant="dark">
+      <tbody>
+        {
+          items.map((items, index)=>{
+            return(
+              <tr key={index}>
+              <td>
+                <div style={{ background:'white',overflow:'hidden',display:'flex', justifyContent:'center', alignItems:'center', height:'8rem'}}
+    >
+                  <div style={{padding:'.5rem'}}>
+<img src={items.image} style={{width:'5rem'}} alt={items.title} />
+                  </div>
+                </div>
+              </td>
+              <td>
+                <h6 style={{whiteSpace:'nowrap', width:'14rem', overflow:'hidden'}}>{items.title}</h6>
+              </td>
+              <td>
+                $. {items.price}
+              </td>
+              <td>Quantity({items.quantity}) </td>
+              <td>
+                <button onClick={()=>updateItemQuantity(items.id, items.quantity - 1)} className='ms-2'>-
+                </button>
+                <button onClick={()=>updateItemQuantity(items.id, items.quantity +1)} className='ms-2'>+
+                </button>
+              </td>
+                {/* <h6 style={{whiteSpace:'nowrap', width:'14rem', overflow:'hidden'}}>{items.title}</h6> */}
+            </tr>
+            )
+          })
+        }
+       
+      </tbody>
+    </Table>
         </Row>
     </Container>
   );
